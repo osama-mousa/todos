5d99475db4a7c521395891df8028ab5c02b50fd5
osamasayd <osamasayd141@gmail.com>
2025-02-22 03:43:34 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Sidebar from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +12,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body
-        className={`${inter.className} dark:bg-gray-900 dark:text-white min-h-screen`}
+        className={`${inter.className} dark:bg-neutral-900 dark:text-neutral-100 min-h-screen`}
       >
-        <div className="flex">
-          <Sidebar />
-          <main className="flex-1 p-8">{children}</main>
-        </div>
+        <main className="p-4 min-h-screen flex flex-col items-center justify-start">
+          {children}
+        </main>
       </body>
     </html>
   );
