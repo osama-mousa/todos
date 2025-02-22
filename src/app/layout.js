46be92785c68700ac4eca0490b41f6c0fae37c91
osamasayd <osamasayd141@gmail.com>
2025-02-22 22:05:27 +0300

    update metadata and set footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,21 +1,32 @@
+import Footer from "@/components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Todo App",
-  description: "Advanced Todo Application",
-};
+  title: 'ToDos',
+  description: 'Next.js Todo Application',
+  manifest: '/manifest.json',
+  icons: {
+    icon: [
+      { url: '/icons/favicon.ico' },
+      { url: '/icons/todos.png', sizes: '192x192', type: 'image/png' },
+    ],
+    apple: '/icons/todos.png',
+  },
+  themeColor: '#171717',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
       <body
-        className={`${inter.className} dark:bg-neutral-900 dark:text-neutral-100 min-h-screen`}
+        className={`${inter.className} dark:bg-neutral-900 dark:text-neutral-100 min-h-screen custom-scrollbar`}
       >
         <main className="p-4 min-h-screen flex flex-col items-center justify-start">
           {children}
+          <Footer />
         </main>
       </body>
     </html>
